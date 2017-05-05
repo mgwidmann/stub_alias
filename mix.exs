cad284f9af0d9b5984b9e51a2b461b2d2f09aad8
Matt Widmann <mgwidmann@gmail.com>
2017-05-05 07:59:29 +0200

    Update dev deps.

--- mix.exs
+++ mix.exs
@@ -39,8 +39,8 @@ defmodule StubAlias.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.10", only: :dev},
-      {:earmark, "~> 0.1", only: :dev}
+      {:ex_doc, "~> 0.15", only: :dev},
+      {:earmark, "~> 1.2", only: :dev}
     ]
   end
 
